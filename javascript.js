578c2904d6aed0bf0a951586591edf9e398c6af7
kskannandotwin <kskannan.win@gmail.com>
2023-02-05 05:18:12 +0530

    05.02.2023
    224. app skeleton and decoration 1 has completed

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 $(function() {
-    $('#slider').slider();
+    $('#slider').slider({
+        min: 3,
+        max: 30,
+        slide: function(event, ui) {
+            $('#circle').width(ui.value);
+            $('#circle').height(ui.value);
+        }
+    });
 });
