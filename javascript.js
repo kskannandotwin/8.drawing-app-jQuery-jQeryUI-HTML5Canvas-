43c123a2aff78d99b35b5b778caca1f3aa63b473
kskannandotwin <kskannan.win@gmail.com>
2023-02-05 13:09:48 +0530

    05.02.2023
    227. learn html5 canvas has completed

--- javascript.js
+++ javascript.js
@@ -7,4 +7,36 @@ $(function() {
             $('#circle').height(ui.value);
         }
     });
+
+    var canvas = document.getElementById('paint');
+    var context = canvas.getContext('2d');
+
+    // draw a line
+    // declare new path
+    context.beginPath();
+
+    // set line width
+    context.lineWidth = 40;
+
+    // set line color
+    context.strokeStyle = '#42e565';
+
+    // set cap to the line (round, butt, square)
+    context.lineCap = 'round';
+
+    // set line join style (bevel, round, miter)
+    context.lineJoin = 'round'; 
+
+    // position the context point
+    context.moveTo(50, 50);
+
+    // draw a straight line from starting point to a new position
+    context.lineTo(200, 200);
+
+    // draw another line
+    context.lineTo(400, 100);
+
+    // make line visible
+    context.stroke();
+
 });
